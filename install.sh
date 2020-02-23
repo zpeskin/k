bd1912cd85d0a32aedee1d7f81181f635f8dd042
Zachary Peskin <zpeskin@csu.fullerton.edu>
2020-02-22 18:56:41 -0800

    commit

--- install.sh
+++ install.sh
@@ -1,1 +1,38 @@
-sudo apt -y install gnome-core kali-defaults kali-root-login desktop-base ssh ffmpeg youtube-dl vlc wget aria2 qbittorrent make transmission git subversion cmake rclone gdebi alien synaptic
+sudo apt update && sudo apt -y upgrade && sudo apt -y autoremove && \
+sudo apt -y install \
+gnome-core \
+kde-plasma-desktop \
+python3 \
+axel \
+rsync \
+xterm \
+mate-core \
+mate-desktop-environment-extra \
+mate-desktop-environment-extras \
+python3-pip \
+npm \
+atom \
+kali-defaults \
+kali-root-login \
+desktop-base \
+ssh \
+ffmpeg \
+youtube-dl \
+vlc \
+wget \
+aria2 \
+qbittorrent \
+make \
+transmission \
+git \
+subversion \
+cmake \
+rclone \
+gdebi \
+alien \
+clang \
+llvm \
+anydesk \
+sublime-text \
+synaptic \
+sudo apt update && sudo apt -y upgrade && sudo apt -y autoremove
